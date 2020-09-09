398ed15e85d975b16e733480d357ec01674fe2bf
Marta Maachowicz <malachowicz.marta@gmail.com>
2020-09-09 15:13:03 +0200

    add trash button

--- scripts/budget.js
+++ scripts/budget.js
@@ -1,5 +1,9 @@
 let incomeArr = [];
 let totalIncomeSum = [];
+const incomeObj = {};
+
+
+// onclick function ######################################################
 
 function onAddBtnClick() {
     let income = document.getElementById('incomeName');
@@ -18,6 +22,47 @@ function onAddBtnClick() {
     console.log(updateIncomeSum ());
 };
 
+function onEditRemoveBtnClick(e) {
+    const item = e.target;
+    let incomeSum = document.getElementById('incomeSum');
+    let index = totalIncomeSum.indexOf(incomeSum.value);
+
+    if (item.classList[0] === 'incomeTrashBtn') {
+        item.parentElement.remove();
+        totalIncomeSum.splice(index, 1);
+        incomeArr.splice(index, 1);
+    };
+
+    // if (item.classList[0] === 'incomeEditBtn') {
+    //     let parent = item.parentElement;
+        
+    //     let li = parent.firstChild;
+    //     li.innerText = ""
+    //     // li.innerHTML = '<input type="text" id="editInputText" placeholder="edycja tekst"><input type="text" id="editInputSum" placeholder="kwota">'       
+         
+    //     let editNameInput = document.createElement('input');
+
+    //     let editPriceInput = document.createElement('input');
+
+    //     li.appendChild(editNameInput)
+    //     li.appendChild(editPriceInput)
+        
+    //     let editInputText = document.getElementById('editInputText');
+    //     editInputText.addEventListener("keyup", function(event) {
+    //         if (event.keyCode === 13) {
+
+    //         }
+    //     })
+    // }
+
+    updateIncomeSum ()
+
+};
+
+
+
+// another function ######################################################
+
 function uptadeIncomeList () {
 
     let ul = document.querySelector('#incomeList');
@@ -28,10 +73,30 @@ function uptadeIncomeList () {
     incomeSum.value = '';
 
     incomeArr.forEach(elem => {
-        let li = document.createElement('li');
-        li.innerText = elem;
+        let divIncome = document.createElement('div');
+        divIncome.classList.add('income-list-item-container');
         let ul = document.querySelector('#incomeList');
-        ul.appendChild(li);
+        ul.appendChild(divIncome);
+
+
+        let li = document.createElement('li');
+        li.classList.add('income-list-item');
+        li.innerText = elem;
+        li.id = incomeArr.indexOf(elem);
+        divIncome.appendChild(li);
+
+        let editBtn = document.createElement('button');
+        editBtn.innerText = 'Edycja';
+        editBtn.classList.add('incomeEditBtn');
+        editBtn.onclick = onEditRemoveBtnClick;
+        editBtn.id = 'incomeEditBtn';
+        divIncome.appendChild(editBtn);
+
+        let trashBtn = document.createElement('button');
+        trashBtn.classList.add('incomeTrashBtn');
+        trashBtn.onclick = onEditRemoveBtnClick;
+        trashBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        divIncome.appendChild(trashBtn);
     });
 };
 
@@ -44,8 +109,8 @@ function updateIncomeSum () {
     });
 
     let totalIncomeSumText = document.getElementById('totalIncomeSumText');
-    totalIncomeSumText.innerText = sum;
+    totalIncomeSumText.innerText = sum + 'zł';
 
-}
+};
 
 
